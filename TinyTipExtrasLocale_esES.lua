5588a8fe2b604d808acc4071c8fa1da16be2c382
generalmx <generalmx@82cf3e7e-d127-0410-9634-436ce4f032e8>
2007-06-17 20:56:13 +0000

    TinyTipExtras: Moved to branches, as this is deprecated.

---
+++ TinyTipExtrasLocale_esES.lua
@@ -0,0 +1,19 @@
+if TinyTipExtrasLocale and TinyTipExtrasLocale == "esES" then
+	-- TinyTipTargets
+	TinyTipTargetsLocale_Targeting		= "Seleccionando Objetivo"
+	TinyTipTargetsLocale_YOU			= "<<T\195\154>>"
+	TinyTipTargetsLocale_TargetedBy	= "Objetivo de"
+
+	TinyTipTargetsLocale_Unknown	= "Desconocido"
+
+	-- TinyTipExtras core
+	TinyTipExtrasLocale_Buffs = "Buffs"
+	TinyTipExtrasLocale_DispellableDebuffs = "Disipable"
+	TinyTipExtrasLocale_DebuffMap = {
+		["Magic"] = "|cFF5555FFMagia|r",
+		["Poison"] = "|cFFFF5555Veneno|r",
+		["Curse"] = "|cFFFF22FFMaldici\195\179n|r",
+		["Disease"] = "|cFF555555Enfermedad|r" }
+
+	TinyTipExtrasLocale = nil -- we no longer need this
+end
