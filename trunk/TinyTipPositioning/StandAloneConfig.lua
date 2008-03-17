7a5919b440b32eb25b7f69f58b70947087dc43ae
generalmx <generalmx@82cf3e7e-d127-0410-9634-436ce4f032e8>
2008-03-17 05:12:11 +0000

    TinyTipPositioning: Added ability to position frame and mouseover units differently in combat, which resulted in further optimization of code. Also added "EtcAnchor" for non-GameTooltip tooltips which use GameTooltipDefaultAnchor.

--- trunk/TinyTipPositioning/StandAloneConfig.lua
+++ trunk/TinyTipPositioning/StandAloneConfig.lua
@@ -10,7 +10,6 @@ if not TinyTipDB or not TinyTipDB.configured then
                 -- To select the TinyTip default, set the value to nil.
                 --]]
                 ["FAnchor"] = nil,           -- "BOTTOMRIGHT", "BOTTOMLEFT", "TOPRIGHT", "TOPLEFT", "CURSOR", "SMART"
-                                             -- If "HIDDEN" is given, then the tooltip will NOT be shown.
                                              -- Used only in Frames. TinyTip default (when nil) is BOTTOMRIGHT.
                                              -- "SMART" anchor will attempt to position the tooltip as to not obscure frames.
                 ["FCursorAnchor"] = nil,     -- Which side of the cursor to anchor for frame units.
@@ -18,10 +17,27 @@ if not TinyTipDB or not TinyTipDB.configured then
                 ["MAnchor"] = nil,           -- Used only for Mouseover units. Options same as above, with the
                                              -- addition of "GAMEDEFAULT". TinyTip Default is CURSOR.
                 ["MCursorAnchor"] = nil,     -- Which side of the cursor to anchor for mouseover (world).
-                ["FOffX"] = nil,             -- X offset for Frame units (horizontal).
-                ["FOffY"] = nil,             -- Y offset for Frame units (vertical).
-                ["MOffX"] = nil,             -- Offset for Mouseover units (World Frame).
-                ["MOffY"] = nil              -- "     "       "       "       "
+                ["FOffX"] = nil,             -- X offset for Frame units (horizontal). By default (nil), this is not used.
+                ["FOffY"] = nil,             -- Y offset for Frame units (vertical). By default (nil), this is not used.
+                ["MOffX"] = nil,             -- Offset for Mouseover units (World Frame). By default (nil), this is not used.
+                ["MOffY"] = nil,             -- "     "       "       "       "
+                ["CFAnchor"] = nil,          -- "GAMEDEFAULT", "BOTTOMRIGHT", "BOTTOMLEFT", "TOPRIGHT", "TOPLEFT", "CURSOR", "SMART"
+                                             -- Anchor for frames when in combat. Overrides other settings.
+                                             -- By default (nil), this is not used.
+                ["CFOffX"] = nil,            -- Offset for frame units when in combat. Overrides other settings.
+                                             -- By default (nil), this is not used.
+                ["CFOffY"] = nil,            -- "     "       "       "       "
+                ["MFAnchor"] = nil,          -- "GAMEDEFAULT", "BOTTOMRIGHT", "BOTTOMLEFT", "TOPRIGHT", "TOPLEFT", "CURSOR"
+                                             -- Anchor for mouseover units when in combat. Overrides other settings.
+                                             -- By default (nil), this is not used.
+                ["CMOffX"] = nil,            -- Offset for Mouseover units when in combat. Overrides other settings.
+                                             -- By default (nil), this is not used.
+                ["CMOffY"] = nil,            -- "     "       "       "       "
+                ["EtcAnchor"] = nil,         -- "GAMEDEFAULT", "BOTTOMRIGHT", "BOTTOMLEFT", "TOPRIGHT", "TOPLEFT", "CURSOR", "SMART"
+                                             -- Used for all tooltips -except- GameTooltip. By default (nil), this is not used.
+                                             -- Beware this feature, it can get messy.
+                ["EtcOffX"] = nil,           -- Offset X for non-GameTooltip tooltips. Beware. Beeewaaarrre.
+                ["EtcOffY"] = nil,           -- "           "           "           "           "
     }
 
     TinyTip_StandAloneDB =  (TinyTip_StandAloneDB and setmetatable(t, {__index=TinyTip_StandAloneDB})) or t
